37aba6f75dccf51492caeee1752a3993914563ce
Aryaman <aryamanyash11@gmail.com>
2024-03-17 12:10:02 +0530

    listing api

--- listing.js
+++ listing.js
@@ -9,17 +9,18 @@ const client = new MongoClient(uri, {
 });
 
 module.exports = async function(req,res){
-    console.log(">>>>>>> query",req.query);
+    
     let country = req.query.country;
   try{
-
-      console.log(">>>>>>> query")
-   let data = await client.db("sample_airbnb").collection("listingsAndReviews").aggregate([
+    let data = []
+      if(Object.keys(req.query).length == 0){
+        console.log(">>>>>>> query -----1");
+         let data = await client.db("sample_airbnb").collection("listingsAndReviews").aggregate([
     {
-      $match: {
-        "address.country": country
-      }
-    },{
+      $match : {"minimum_nights":  "1"}
+    },
+          {
+      
       $project: {
         _id: 1,
         name:1,
@@ -53,16 +54,69 @@ module.exports = async function(req,res){
         review_count:"$number_of_reviews",
       }
     }
-  ]).toArray()
+       ]).limit(10).toArray()
+      
+        res.json({
+        status: "200",
+        message: "success",
+        response: data
+        })
+
+      }
+      else if(country){
+        let data = await client.db("sample_airbnb").collection("listingsAndReviews").aggregate([
+    {
+      $match: {
+        "address.country": country
+      }
+      },{
+      $project: {
+        _id: 1,
+        name:1,
+        summary:1,
+        overall_rating:{
+               $avg: [
+                "$review_scores.review_scores_accuracy", 
+                "$review_scores.review_scores_cleanliness", 
+                "$review_scores.review_scores_checkin", 
+                "$review_scores.review_scores_communication", 
+                "$review_scores.review_scores_location", 
+                "$review_scores.review_scores_value"
+            ]
+            },
+        price:{
+                $convert:{
+                    input: "$price",
+                    to: "double",
+                    onError: 0.0,  // If conversion fails, return 0.0
+                    onNull: 0.0    // If field is null, return 0.0
+                }
+            },
+        tax: {
+                $convert: {
+                input: "$cleaning_fee",
+                to: "double",
+                onError: 0.0,  // If conversion fails, return 0.0
+                onNull: 0.0    // If field is null, return 0.0
+                }},
+        photo : "$images.picture_url",
+        review_count:"$number_of_reviews",
+      }
+     }
+     ]).toArray()
+
+      res.json({
+      status: "200",
+      message: "success",
+      response: data
+      })
+
+      }
 
 
 
-  res.json({
-    status: "200",
-    message: "success",
-    response: data
-  })
-  }catch{
-   console.log(">>>>>>> query")
+ 
+  }catch(err){
+   console.log(">>>>>>> error",err)
   }
 }
