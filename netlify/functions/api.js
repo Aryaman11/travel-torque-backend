e8a89e0349eb52dbee05089c8ccf53ec47fe6521
Aryaman <aryamanyash11@gmail.com>
2024-06-18 20:43:11 +0530

    changes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const app = express();
-const home_url = require('./home');
-const detail = require('./detail');
-const checkout = require('./checkout');
-const listing = require('./listing');
+const home_url = require('../../home');
+const detail = require('../../detail');
+const checkout = require('../../checkout');
+const listing = require('../../listing');
 
 const cors = require('cors'); 
+const ServerlessHttp = require('serverless');
 app.use(cors());
 // app.use(express.static(path.join(__dirname, "dist")));
 
 
 
-app.get('/api/home', home_url);
-app.get('/api/detail', detail);
-app.get('/api/checkout', checkout);
-app.get('/api/listing', listing);
+app.get('/home', home_url);
+app.get('/detail', detail);
+app.get('/checkout', checkout);
+app.get('/listing', listing);
 
 
 
@@ -27,3 +28,4 @@ if (!module.parent) {
   app.listen(3000);
   console.log('Express started on port 3000');
 }
+export const handler = Serverless(app);
